16d6e2c5a4eb3fb5c05450a5485286d9cd873039
GabrielN06 <gabrielnunesdacostaanselmo@gmail.com>
2025-02-24 14:43:01 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -3,25 +3,25 @@ Console.WriteLine("-----Minhas Frutas Prefiridas-----\n");
 
 Console.ForegroundColor = ConsoleColor.White;
 Console.BackgroundColor = ConsoleColor.Yellow;
-Console.WriteLine("Banana\n");
+Console.WriteLine("1. Banana\n");
 Console.ResetColor();
 
 Console.ForegroundColor = ConsoleColor.Red;
 Console.BackgroundColor = ConsoleColor.DarkRed;
-Console.WriteLine("Maca\n");
+Console.WriteLine("2. Maca\n");
 Console.ResetColor();
 
 Console.ForegroundColor = ConsoleColor.Green;
 Console.BackgroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine("Pera\n");
+Console.WriteLine("3. Pera\n");
 Console.ResetColor();
 
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.BackgroundColor = ConsoleColor.DarkMagenta;
-Console.WriteLine("Uva\n");
+Console.WriteLine("4. Uva\n");
 Console.ResetColor();
 
 Console.ForegroundColor = ConsoleColor.Red;
 Console.BackgroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine("Melancia\n");
+Console.WriteLine("5. Melancia\n");
 Console.ResetColor();
