661986076d842ff2cc19250a8868813c5dc0cf31
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-02-24 14:38:42 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine("-----Minhas Frutas Prefiridas-----\n"); 
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Banana\n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Maca\n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Pera\n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Uva\n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Melancia\n");
+Console.ResetColor();
